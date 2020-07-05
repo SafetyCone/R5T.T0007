1f6addd056b7881224fe28210d45665681bcc51c
MinexAutomation <minex.automation@gmail.com>
2020-07-04 22:30:20 -0700

    Added MachineName class.

---
+++ source/R5T.T0007/Code/Class/MachineName.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+using R5T.Stagira;
+
+
+namespace R5T.T0007
+{
+    public class MachineName : TypedString
+    {
+        public MachineName(string value)
+            : base(value)
+        {
+        }
+    }
+}
--- source/R5T.T0007/Code/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace R5T.T0007
-{
-    public class Class1
-    {
-    }
-}
