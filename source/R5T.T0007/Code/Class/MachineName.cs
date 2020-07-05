6d9fda9dc8901f168254946e3d188f43d57ebce8
MinexAutomation <minex.automation@gmail.com>
2020-07-04 23:03:26 -0700

    Added String extensions.

--- source/R5T.T0007/Code/Class/MachineName.cs
+++ source/R5T.T0007/Code/Class/MachineName.cs
@@ -7,6 +7,17 @@ namespace R5T.T0007
 {
     public class MachineName : TypedString
     {
+        #region Static
+
+        public static MachineName From(string value)
+        {
+            var machineName = new MachineName(value);
+            return machineName;
+        }
+
+        #endregion
+
+
         public MachineName(string value)
             : base(value)
         {
---
+++ source/R5T.T0007/Code/Extensions/StringExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+namespace R5T.T0007
+{
+    public static class StringExtensions
+    {
+        public static MachineName AsMachineName(this string value)
+        {
+            var machineName = new MachineName(value);
+            return machineName;
+        }
+    }
+}
